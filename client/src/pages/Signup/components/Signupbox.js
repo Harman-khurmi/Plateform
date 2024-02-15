090a0361e8a161a04127d67d5989d40041dc8481
Shubham Gandhi <shubhamgandhi1002@gmail.com>
2024-02-15 17:14:08 +0530

    error array

--- client/src/pages/Signup/components/Signupbox.js
+++ client/src/pages/Signup/components/Signupbox.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useState } from 'react';
 import { Link } from "react-router-dom";
+
+
 export default function Signup() {
   const [user, setUser] = useState({
     name: "",
