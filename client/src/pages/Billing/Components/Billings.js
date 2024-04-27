4e53fccd7a9c7b0edd318fe8daf391391dc1c546
isha-03 <ijain2_be21@thapar.edu>
2024-04-28 01:05:03 +0530

    billing

--- client/src/pages/Billing/Components/Billings.js
+++ client/src/pages/Billing/Components/Billings.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import authUtils from '../../../utils/jwtRollNumber';
-
+import './billings.css';
 const BillingPage = () => {
     const [bookings, setBookings] = useState([]);
     const [totalAmount, setTotalAmount] = useState(0);
@@ -45,8 +45,87 @@ const BillingPage = () => {
         fetchBookings();
     }, []);
 
+    var accbal=22000;
+    var totalMeals=10;
+    var breakfast=3;
+    var lunch=4;
+    var dinner=3;
+    var total=25000;
+    var day=new Date();
     return (
-        <div>
+        <>
+        <div className="container shadow">
+      
+        <div className="vertical-box">
+            <div className="box shadow">
+            
+                <div className="vertical-box">
+                <div className="heading1">Today {day.toLocaleDateString()}</div>
+                <div className="flex-container">
+                    <div className='flex-item'>SNo.</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Date</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Meal Name</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Price per Meal</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Amount per Meal</div>
+                </div>
+                <div className="flex-container">
+                    <div className='flex-item'>SNo.</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Date</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Meal Name</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Price per Meal</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Amount per Meal</div>
+                </div>
+               
+                <div className="flex-container">
+                    <div className='flex-item'>SNo.</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Date</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Meal Name</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Price per Meal</div>
+                        {/* <span className="space4"></span> */}
+                    <div className='flex-item'>Amount per Meal</div>
+                </div>
+                </div>
+                   
+                <div className="vertical-box">
+                    {bookings.map((booking, index) => (
+                        <div key={index} className='flex-container'>
+                            <div className='flex-item'>{index + 1}</div>
+                            <div className='flex-item'>{new Date(booking.bookingDate).toLocaleDateString('en-GB')}</div>
+                            <div className='flex-item'>{booking.class.className}</div>
+                            <div className='flex-item'>75</div>
+                            <div className='flex-item'>{booking.status === 'present' ? `+75` : `+10 fine`}</div>
+                        </div>
+                    ))}
+                </div>
+           
+            </div>
+            
+        </div>
+        <div className="blue-box">
+            <h2>Account Balance</h2>
+            <p style={{fontSize:50}}>Rs {accbal}</p>
+            <p style={{fontSize:20, marginTop:30}}>Total meals taken: <span className="space"> </span>{totalMeals}</p>
+            <div className="line"></div>
+            <p style={{fontSize:20, marginTop:30}}>Breakfast Count: <span className="space"> </span>{breakfast}</p>
+            <p style={{fontSize:20}}>Lunch Count: <span className="space2"> </span>{lunch}</p>
+            <p style={{fontSize:20}}>Dinner Count: <span className="space3"> </span>{dinner}</p>
+            <div className="d-flex justify-content-center mt-5">
+            <span className="total">Total Paid: Rs {total}</span>
+            </div>
+        </div>
+        </div>
+        {/* <div>
             <h1>Billing History</h1>
             <table>
                 <thead>
@@ -71,7 +150,8 @@ const BillingPage = () => {
                 </tbody>
             </table>
             <p>Total Amount Till Date: {totalAmount} Rupees</p>
-        </div>
+        </div> */}
+        </>
     );
 };
 
